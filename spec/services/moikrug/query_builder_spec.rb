77c876bf1436ff625d4620efa81b1ff88932bffd
Alexey Blinov <nilcolor@gmail.com>
2015-01-10 21:46:15 +0300

    Fixed time-affected tests

--- spec/services/moikrug/query_builder_spec.rb
+++ spec/services/moikrug/query_builder_spec.rb
@@ -1,4 +1,12 @@
 describe Moikrug::QueryBuilder do
+  before :all do
+    Timecop.freeze(Time.zone.local(2014, 8, 1, 8, 0, 0))
+  end
+
+  after :all do
+    Timecop.return
+  end
+
   let(:options) do
     { city: "москва",
       specializations: ["ловить бабочек"],
@@ -8,21 +16,21 @@ describe Moikrug::QueryBuilder do
 
   it do
     builder = Moikrug::QueryBuilder.new(options)
-    example = 'москва "ищу работу" ("ноября 2014" OR "октября 2014") "о себе" (ловить бабочек)'
+    example = 'москва "ищу работу" ("августа 2014" OR "июля 2014") "о себе" (ловить бабочек)'
 
     expect(builder.call).to eq(example)
   end
 
   it do
     builder = Moikrug::QueryBuilder.new(options.merge specializations: ["ловить", "бабочек"])
-    example = 'москва "ищу работу" ("ноября 2014" OR "октября 2014") "о себе" (ловить OR бабочек)'
+    example = 'москва "ищу работу" ("августа 2014" OR "июля 2014") "о себе" (ловить OR бабочек)'
 
     expect(builder.call).to eq(example)
   end
 
   it do
     builder = Moikrug::QueryBuilder.new(options.merge period: 1)
-    example = 'москва "ищу работу" ("ноября 2014") "о себе" (ловить бабочек)'
+    example = 'москва "ищу работу" ("августа 2014") "о себе" (ловить бабочек)'
 
     expect(builder.call).to eq(example)
   end
